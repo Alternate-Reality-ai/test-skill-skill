336d7a4cdf0b5ff11d5ae42375459a0fb3beb363
Alternate-Reality-ai <76701567+Alternate-Reality-ai@users.noreply.github.com>
2020-12-29 12:14:55 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,19 @@ from mycroft import MycroftSkill, intent_file_handler
 
 class TestSkill(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        """ The __init__ method is called when the Skill is first constructed.
+        It is often used to declare variables or perform setup actions, however
+        it cannot utilise MycroftSkill methods as the class does not yet exist.
+        """
+        super().__init__()
+        self.learning = True
+
+    def initialize(self):
+        """ Perform any final setup needed for the skill here.
+        This function is invoked after the skill is fully constructed and
+        registered with the system. Intents will be registered and Skill
+        settings will be available."""
+        my_setting = self.settings.get('my_setting')
 
     @intent_file_handler('skill.test.intent')
     def handle_skill_test(self, message):
